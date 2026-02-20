25f6bf2ea1101f3731e702e588df325d1cbd956d
Nathan Dawson <nate@Nathans-MacBook-Pro.local>
2026-02-20 18:52:24 +0000

    message

--- src/lib/pilotAllowlist.ts
+++ src/lib/pilotAllowlist.ts
@@ -1,4 +1,12 @@
 export const PILOT_EMAILS = [
   'bigogita@gmail.com',
-  'nddawson15@gmail.com'
+  'nddawson15@gmail.com',
+  'mickid3@gmail.com',
+  't.walls3806@gmail.com',
+  'Nahsspncr@gmail.com ',
+  'officialjt555@gmail.com',
+  'Anna@strangeaddiction.co.uk',
+   'Kofficialldn@gmail.com',
+   'vanda500@hotmail.com',
+   'tallulah.music@hotmail.com',
 ]
